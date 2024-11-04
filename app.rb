d863750b36ce3944cd28b27c27aada224b3d202f
ManneMyten <manne.laurin@elev.ga.ntig.se>
2024-11-04 14:55:35 +0100

    lagt till lite grejer

--- app.rb
+++ app.rb
@@ -1,7 +1,26 @@
 class App < Sinatra::Base
 
+    def db
+		return @db if @db
+
+		@db = SQLite3::Database.new("db/media.sqlite")
+		@db.results_as_hash = true
+
+		return @db
+	end
+
     get '/' do
+        redirect('/media')
+    end
+
+    get '/media' do
+        @media = db.execute('SELECT * FROM media')
         erb(:"index")
+    end
+
+    get '/media/:id' do |id|
+        @media = db.execute('SELECT * FROM media WHERE id=?', id)
+        erb(:"media/show")
     end
 
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -2,10 +2,36 @@ require 'sqlite3'
 
 class Seeder
 
-  def self.seed!
-    p "doit"
-  end
+    def self.seed!
+        drop_tables
+        create_tables
+        populate_tables
+    end
 
+    def self.drop_tables
+        db.execute('DROP TABLE IF EXISTS media')
+    end
+
+    def self.create_tables
+        db.execute('CREATE TABLE media (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    title TEXT NOT NULL,
+                    description TEXT)')
+    end
+
+    def self.populate_tables
+        db.execute('INSERT INTO media (title, description) VALUES ("Alien", "After investigating a mysterious transmission of unknown origin, the crew of a commercial spacecraft encounters a deadly lifeform.")')
+        db.execute('INSERT INTO media (title, description) VALUES ("John Wick", "An assassin returns to his old job after his dog dies.")')
+        db.execute('INSERT INTO media (title, description) VALUES ("The Dark Knight", "Orphan fights a clown.")')
+    end
+
+    private
+    def self.db
+        return @db if @db
+        @db = SQLite3::Database.new('db/media.sqlite')
+        @db.results_as_hash = true
+        @db
+    end
 end
 
 Seeder.seed!
