bf42f348fd1a2651403f42568738888d6495ac09
ManneMyten <manne.laurin@elev.ga.ntig.se>
2024-11-11 14:52:40 +0100

    11 november

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 class App < Sinatra::Base
 
-    def db
+  def db
 		return @db if @db
 
 		@db = SQLite3::Database.new("db/media.sqlite")
@@ -18,9 +18,26 @@ class App < Sinatra::Base
         erb(:"index")
     end
 
+    post '/media' do
+      p params
+      db.execute('INSERT INTO media (title, description) VALUES (?,?)', [params['title'], params['description']])
+      redirect('/media')
+    end
+
+    post '/media/:id/delete' do |id|
+      db.execute('DELETE FROM media WHERE id=?', id)
+      redirect('/media')
+    end
+
+    get '/media/new' do 
+      erb(:"media/new")
+    end
+
     get '/media/:id' do |id|
-        @media = db.execute('SELECT * FROM media WHERE id=?', id)
+        @media = db.execute('SELECT * FROM media WHERE id=?', id).first
+        #todo: om media inte finns gör nåt (.nil?)
         erb(:"media/show")
     end
+
 
 end
